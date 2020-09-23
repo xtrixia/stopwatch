0bcc62f0545aa8f879f7bf624f4533dadf7137b3
Kukuh Sulistyo <kukuh.aji@ruangguru.com>
2020-09-23 23:06:50 +0700

    Optimize things

--- src/App.js
+++ src/App.js
@@ -1,79 +1,23 @@
 import React, { useEffect, useState } from "react";
+import {  BrowserRouter as Router, Route} from "react-router-dom";
+
+import { createBrowserHistory } from "history";
+
+import Stopwatch from "./Stopwatch";
 import "./App.css";
 
-function App() {
-  const initialStopwatch = {
-    hours: 0,
-    minutes: 0,
-    seconds: 0,
-  };
 
-  const [stopwatch, setStopwatch] = useState(initialStopwatch);
+const history = createBrowserHistory();
 
-  const [isTicking, setIsTicking] = useState(false);
-
-  const handleStart = () => {
-    setIsTicking(true);
-  };
-
-  const handleReset = () => {
-    setIsTicking(false);
-    setStopwatch(initialStopwatch);
-  };
-
-  useEffect(() => {
-    let interval = null;
-
-    if (isTicking) {
-      interval = setInterval(() => {
-        let secs = stopwatch.seconds + 1;
-
-        if (secs >= 60) {
-          setStopwatch({
-            ...stopwatch,
-            seconds: 0,
-            minutes: stopwatch.minutes + 1,
-          });
-        } else if (stopwatch.minutes >= 60) {
-          setStopwatch({
-            ...stopwatch,
-            hours: stopwatch.hours + 1,
-            minutes: 0,
-          });
-        } else {
-          setStopwatch({
-            ...stopwatch,
-            seconds: secs,
-          });
-        }
-      }, 1000);
-    } else {
-      clearInterval(interval);
-    }
-    return () => clearInterval(interval);
-  }, [stopwatch, isTicking]);
-
+export default function App() {
   return (
-    <div className='App'>
-      <div className='Paper'>
-        <h1 className='Title'>
-          told you it's <i style={{ color: "#f4f4" }}>just</i> a friggin'
-          stopwatch
-        </h1>
-        <div className='Timer'>
-          <h1 className='Time'>
-            {stopwatch.hours}.{stopwatch.minutes}
-          </h1>
-          <h2 className='Counter'>{stopwatch.seconds}s</h2>
-        </div>
-
-        <div className='Actions'>
-          <button onClick={handleStart}>Start</button>
-          <button onClick={handleReset}>Reset</button>
-        </div>
-      </div>
-    </div>
+    <Router history={history}>
+      <Route exact path="/">
+        <Stopwatch />
+      </Route>
+      <Route path="/news">
+        <h1>404</h1>
+      </Route>
+    </Router>
   );
 }
-
-export default App;
---
+++ src/Stopwatch.js
@@ -0,0 +1,70 @@
+import React, { useEffect, useState, useRef } from "react";
+import { Link } from "react-router-dom";
+
+import "./App.css";
+
+function Stopwatch(setRoute) {
+  const timerRef = useRef();
+  const [seconds, setSeconds] = useState(0);
+
+  const [isTicking, setIsTicking] = useState(false);
+
+  const handleReset = () => {
+    setIsTicking(false);
+    setSeconds(0);
+  };
+
+  useEffect(() => {
+    if (isTicking) {
+      // start interval
+      timerRef.current = setInterval(
+        () => setSeconds((seconds) => seconds + 1),
+        1000
+      );
+    } else {
+      clearInterval(timerRef.current);
+    }
+  }, [isTicking]);
+
+  useEffect(() => {
+    // clear the interval on before unmount
+    return () => clearInterval(timerRef.current);
+  }, []);
+
+  let [h, m, s] = secondToTime(seconds);
+
+  return (
+    <div className="App">
+      <div className="Paper">
+        <h1 className="Title">
+          told you it's <i style={{ color: "#f4f4" }}>just</i> a friggin'
+          stopwatch
+        </h1>
+        <Link to="/news">GOOGOGOO</Link>
+
+        <div className="Timer">
+          <h1 className="Time">{`${h}.${m}`}</h1>
+          <h2 className="Counter">{`${s}s`}</h2>
+        </div>
+
+        <div className="Actions">
+          <button onClick={() => setIsTicking(!isTicking)}>
+            {isTicking ? "Stop" : "Start"}
+          </button>
+          <button onClick={handleReset}>Reset</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Stopwatch;
+
+const secondToTime = (seconds) => {
+  let h = Math.floor(seconds / 3600);
+  let modH = seconds % 3600;
+  let m = Math.floor(modH / 60);
+  let s = modH % 60;
+
+  return [h, m, s];
+};
