d439144386a7ed24c785ba0fab8dddc00d116ae3
Annisa Feryannie <aferyannie@gmail.com>
2020-09-23 19:41:17 +0700

    Update unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders ui", () => {
+  render(<App />);
+  // check button `Start` to be exist
+  expect(screen.getByText("Start", { exact: true }));
+  // check button `Reset` to be exist
+  expect(screen.getByText("Reset", { exact: true }));
 });
